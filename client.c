bc822c86c16f412820caa9468484a856225086c2
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-09-30 01:19:13 -0400

    Did some initial groundwork on client and server

--- client.c
+++ client.c
@@ -2,3 +2,16 @@
 /* CS 3251, Project 2 */
 /* client.c */
 
+#include <stdio.h>
+#include <sys/socket.h>
+
+/* Constants */
+
+/* Function pointers */
+
+/* The main function */
+int main(int argc, char *argv[])
+{
+	/* Command line interface logic */
+
+}
