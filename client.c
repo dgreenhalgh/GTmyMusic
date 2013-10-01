0dea4988ea75a43d0ab63ddc5de377f7a4aed377
David Greenhalgh <david@david-ThinkPad-X220.(none)>
2013-09-30 22:41:48 -0400

    Added directCall command parsing

--- client.c
+++ client.c
@@ -4,14 +4,62 @@
 
 #include <stdio.h>
 #include <sys/socket.h>
+#include <string.h>
 
 /* Constants */
+#define LIST 0
+#define DIFF 1
+#define PULL 2
+#define LEAVE 3
+
+/* Strings.xml */
+char* commands[] = {"LIST", "DIFF", "PULL", "LEAVE"};
+
+const char* badCmd = "Command not recognized\n";
 
 /* Function pointers */
+int directCall(int);
 
 /* The main function */
 int main(int argc, char *argv[])
 {
-	/* Command line interface logic */
+	//printf("%d", argc);
+	/* Command line interface */
+	if(argc == 1)
+	{
 
+	}
+
+	/* Direct command */
+	if(argc == 2)
+	{
+		if(strcmp(argv[1], "list") == 0)
+		{
+			directCall(LIST);
+		}
+		else if(strcmp(argv[1], "diff") == 0)
+		{
+			directCall(DIFF);
+		}
+		else if(strcmp(argv[1], "pull") == 0)
+		{
+			directCall(PULL);
+		}
+		else if(strcmp(argv[1], "leave") == 0)
+		{
+			directCall(LEAVE);
+		}
+		else
+		{
+			printf("%s", badCmd);
+		}
+
+
+	}
+}
+
+int directCall(int cmd)
+{
+	char* userCommand = commands[cmd];
+	printf("%s", userCommand);
 }
