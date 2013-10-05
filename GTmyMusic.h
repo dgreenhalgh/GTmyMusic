8f56c368bb9064fe8041825be59b5dca4112dad5
Devin Gonzalez <devin.m.gonzalez@gmail.com>
2013-10-05 18:50:21 -0400

    Moved library definitions from server.c and client.c to GTmyMusic.h

--- GTmyMusic.h
+++ GTmyMusic.h
@@ -10,6 +10,17 @@
 #ifndef GTmyMusic	/* Include guard */
 #define GTmyMusic
 
+	/* Included libraries */
+	#include <stdio.h>          /* for printf() and fprintf() */
+	#include <sys/socket.h>     /* for socket(), connect(), send(), and recv() */
+	#include <arpa/inet.h>      /* for sockaddr_in and inet_addr() */
+	#include <stdlib.h>         /* supports all sorts of functionality */
+	#include <unistd.h>         /* for close() */
+	#include <string.h>         /* support any string ops */
+	#include <openssl/evp.h>    /* for OpenSSL EVP digest libraries/SHA256 */
+	#include <pthread.h>		/* for parallel processing */
+
+
 	/* Message Structures */
 	typedef struct list_message {
 
