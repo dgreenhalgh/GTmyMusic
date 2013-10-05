93d2b17c4e8434b28204b86589e8f9b477c661fb
Devin Gonzalez <devin.m.gonzalez@gmail.com>
2013-10-05 18:32:13 -0400

    Initialized header file will empty structures.

---
+++ GTmyMusic.h
@@ -0,0 +1,32 @@
+/*///////////////////////////////////////////////////////////
+*
+* FILE:     	GTmyMusic.h
+* AUTHOR:   	Devin Gonzalez & David Greenhalgh
+* PROJECT:  	CS 3251 Project 2 - Professor Traynor
+* DESCRIPTION:  Custom header file for GTmyMusic.
+*
+*////////////////////////////////////////////////////////////
+
+#ifndef GTmyMusic	/* Include guard */
+#define GTmyMusic
+
+	/* Message Structures */
+	typedef struct list_message {
+
+	};
+
+	typedef struct diff_message {
+
+	};
+
+	typedef struct pull_message {
+
+	};
+
+	typedef struct leave_message {
+
+	};
+
+	/* Other stuff */
+
+#endif
